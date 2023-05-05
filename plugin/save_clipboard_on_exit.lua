506d6970026e1c2e59a42ecdc7378fe908da8a0b
Yuta Katayama <8683947+yutkat@users.noreply.github.com>
2023-05-06 05:56:46 +0900

    Update development environment

--- plugin/save_clipboard_on_exit.lua
+++ plugin/save_clipboard_on_exit.lua
@@ -1,16 +1,16 @@
 if vim.g.save_clipboard_on_exit ~= nil then
-  return
+	return
 end
 vim.g.save_clipboard_on_exit = 1
 
 local groupname = "save_clipboard_on_exit"
 vim.api.nvim_create_augroup(groupname, { clear = true })
 
-vim.api.nvim_create_autocmd({ "VimLeave"}, {
+vim.api.nvim_create_autocmd({ "VimLeave" }, {
 	group = groupname,
 	pattern = "*",
 	callback = function()
-    require('save_clipboard_on_exit').save()
+		require("save-clipboard-on-exit").save()
 	end,
-	once = false,
+	once = true,
 })
